03834dc786eef6c599bc1a14da8544f956f29cdd
Arjun VK <125860492+arjunvk007@users.noreply.github.com>
2025-04-10 16:54:17 -0400

    Delete pages/Heatmap.py

--- pages/Heatmap.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from utils.load_data import load_data
-from utils.visuals import plot_heatmap
-
-def show():
-    df = load_data()
-    st.subheader("🔍 Correlation Heatmap")
-    plot_heatmap(df)
